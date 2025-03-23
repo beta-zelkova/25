e3b07c1b374723b387eb3318bf7ba11407b475ae
beta-zelkova <a.beta.zelkova@gmail.com>
2025-03-24 01:30:35 +0900

    Add files via upload

--- python update_json.py
+++ python update_json.py
@@ -3,6 +3,7 @@ import pandas as pd
 # ファイル名の設定
 EXCEL_FILE = "成績表.xlsx"
 JSON_FILE_DETAIL = "DETAIL_DATA.json"
+JSON_FILE = "batting_data.json"
 
 # 丸数字リスト
 rank_symbols = ["①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩"]
@@ -27,6 +28,7 @@ def update_json():
     try:
         # Excel のデータを読み込む
         df2 = pd.read_excel(EXCEL_FILE, sheet_name="打撃詳細")
+        df = pd.read_excel(EXCEL_FILE, sheet_name="打撃")
 
         # 確認用: 読み込んだデータのカラム一覧を表示
         print("読み込んだカラム:", df2.columns.tolist())
@@ -40,8 +42,9 @@ def update_json():
 
         # JSON, CSV 出力
         df2.to_json(JSON_FILE_DETAIL, orient="records", force_ascii=False, indent=4)
+        df.to_json(JSON_FILE, orient="records", force_ascii=False, indent=4)
 
-        print("DETAIL_DATA.json を更新しました。")
+        print("jsonファイルを更新しました。")
 
     except Exception as e:
         print(f"エラー発生: {e}")
